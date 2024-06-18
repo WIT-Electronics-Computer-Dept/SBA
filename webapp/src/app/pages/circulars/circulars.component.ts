41d6ac595a0d1faf090952dc0bdff7517f92c72f
AbhiMophare <avmophare.work@gmail.com>
2024-06-18 21:02:57 +0530

    created service and components along with folder structure

---
+++ webapp/src/app/pages/circulars/circulars.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circulars',
+  standalone: true,
+  imports: [],
+  templateUrl: './circulars.component.html',
+  styleUrl: './circulars.component.css'
+})
+export class CircularsComponent {
+
+}
---
+++ webapp/src/app/shared-components/alpha-sort/alpha-sort.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alpha-sort',
+  standalone: true,
+  imports: [],
+  templateUrl: './alpha-sort.component.html',
+  styleUrl: './alpha-sort.component.css'
+})
+export class AlphaSortComponent {
+
+}
